381e13ecf4495867bc90adcfc4a7ba156b4a3be5
drewburlingame <drewburlingame@gmail.com>
2014-03-27 02:37:48 -0700

    add AlterData options

---
+++ MultiCommandConsole/Commands/AlterDataOptionsWithDryRun.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace MultiCommandConsole.Commands
+{
+    [ArgSet("AlterData", "Options for commands that can alter data")]
+    public class AlterDataOptionsWithDryRun
+    {
+        [Arg("dryrun", "Log results without applying them.")]
+        public bool IsDryRun { get; set; }
+
+        [Arg("noPrompt", "Prevents the command from prompting the user for confirmation.")]
+        public bool NoPrompt { get; set; }
+
+        public bool ContinueFor(int count, string objectType)
+        {
+            return Continue("You are about to alter data for " + count + " " + objectType + ".  Would you like to continue?");
+        }
+
+        public bool Continue(string prompt = "You are about to alter data.  Would you like to continue?")
+        {
+            if (NoPrompt || IsDryRun)
+            {
+                return true;
+            }
+
+            Console.WriteLine(prompt + " type 'y' or 'yes' to continue");
+            var response = Console.ReadLine();
+            return response != null
+                   && (
+                          response.Equals("y", StringComparison.OrdinalIgnoreCase)
+                          || response.Equals("yes", StringComparison.OrdinalIgnoreCase)
+                      );
+        }
+    }
+}
